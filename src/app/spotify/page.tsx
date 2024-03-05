0dbe7ef8a6481e0bdb412c5c9421b4354a92465a
Hans <hansbala@hansbala.com>
2024-03-04 20:59:16 -0800

    playlist view for spotify

--- src/app/spotify/page.tsx
+++ src/app/spotify/page.tsx
@@ -8,7 +8,11 @@ export default function SpotifyPage() {
 
   const playlists = useMemo(() => {
     return data?.items.map((playlist) => {
-      return playlist.name
+      return {
+        name: playlist.name,
+        url: playlist.uri,
+        image: playlist.images[0]
+      }
     })
   }, [data])
 
@@ -19,8 +23,17 @@ export default function SpotifyPage() {
   return (
     <>
       <h1>Current Playlists</h1>
-      <div>
-        {JSON.stringify(playlists, null, 2)}
+      <div className="flex flex-col gap-5">
+        {
+          playlists?.map((playlist) => (
+            <div key={playlist.url} className="flex flex-row gap-3 items-center">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img alt="Playlist image" src={playlist.image?.url ?? '/test.img'} width={100} height={100} />
+              <h1>{playlist.name}</h1>
+              <p>{playlist.url}</p>
+            </div>
+          ))
+        }
       </div>
     </>
   )
