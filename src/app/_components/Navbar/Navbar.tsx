21a52079b0a24560fb5db6e2849dd1ef1da1cb88
Hans <hansbala@hansbala.com>
2024-03-10 18:55:44 -0700

    add heroicons package and redesign navbar

--- src/app/_components/Navbar/Navbar.tsx
+++ src/app/_components/Navbar/Navbar.tsx
@@ -1,35 +1,64 @@
 import Link from 'next/link'
 import { getServerAuthSession } from '~/server/auth'
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 export default async function Navbar() {
   const session = await getServerAuthSession()
 
   return (
-    <nav className="flex flex-row justify-between bg-white p-5 text-black">
-      <div className="flex flex-row gap-5">
-        <Link href="/home" className="underline">
-          Home
-        </Link>
-        <Link href="/secrets" className="underline">
-          Secrets
-        </Link>
-        <Link href="/spotify" className="underline">
-          Spotify
-        </Link>
-        <Link href="/youtube" className="underline">
-          Youtube
-        </Link>
+    <div className="navbar border-b bg-white text-black">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <Bars3Icon className="h-5 w-5" />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-white p-2 text-black shadow"
+          >
+            <li>
+              <Link href="/home">Home</Link>
+            </li>
+            <li>
+              <Link href="/secrets">Secrets</Link>
+            </li>
+            <li>
+              <Link href="/spotify">Spotify</Link>
+            </li>
+            <li>
+              <Link href="/youtube">Youtube</Link>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Myncer</a>
       </div>
-      {!session && (
-        <Link href="/api/auth/signin" className="underline">
-          Sign In
-        </Link>
-      )}
-      {session && (
-        <Link href="/api/auth/signout" className="underline">
-          Signout
-        </Link>
-      )}
-    </nav>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/secrets">Secrets</Link>
+          </li>
+          <li>
+            <Link href="/spotify">Spotify</Link>
+          </li>
+          <li>
+            <Link href="/youtube">Youtube</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {session ? (
+          <Link href="/api/auth/signout" className="btn">
+            Signout
+          </Link>
+        ) : (
+          <Link href="/api/auth/signin" className="btn">
+            Sign In
+          </Link>
+        )}
+      </div>
+    </div>
   )
 }
