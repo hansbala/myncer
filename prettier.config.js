32ea121eecd5d1cb47bf6fb4e3e29f3b3777a893
Hans <hansbala@hansbala.com>
2024-03-10 18:18:06 -0700

    some more linting changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
   semi: false,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
-};
+}
 
-export default config;
+export default config
