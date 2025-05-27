cd5f2dc2ced7efb4a765914db337fee857b15378
Hans <hansbala@hansbala.com>
2025-05-26 22:39:56 -0700

    add basic dev environment using nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Myncer Dev environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go_1_22
+            pkgs.nodejs_20
+            pkgs.pnpm
+            pkgs.protobuf
+          ];
+
+          shellHook = ''
+            export PATH=$PWD/node_modules/.bin:$PATH
+            echo "🧪 Myncer flake shell ready"
+          '';
+        };
+      });
+}
