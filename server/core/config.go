0acd25c1d278a34014b01b819fc365190d9cad29
Hans <hansbala@hansbala.com>
2025-05-27 19:59:59 -0700

    add server mode

--- server/core/config.go
+++ server/core/config.go
@@ -6,9 +6,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type ServerMode int
+
+const (
+	SERVER_MODE_DEV ServerMode = iota
+	SERVER_MODE_PROD
+)
+
 type Config struct {
 	DatabaseUrl string
 	JwtSecret   string
+	ServerMode  ServerMode
 }
 
 func MustGetConfig() *Config {
@@ -16,8 +24,20 @@ func MustGetConfig() *Config {
 	if err := godotenv.Load(); err != nil {
 		Errorf(WrappedError(err, "failed to load config"))
 	}
+	var serverMode ServerMode
+	serverModeStr := os.Getenv("SERVER_MODE")
+	switch serverModeStr {
+	case "dev":
+		serverMode = SERVER_MODE_DEV
+	case "prod":
+		serverMode = SERVER_MODE_PROD
+	default:
+		// Safer to fallback to production.
+		serverMode = SERVER_MODE_PROD
+	}
 	return &Config{
 		DatabaseUrl: os.Getenv("DB_URL"),
 		JwtSecret:   os.Getenv("JWT_SECRET"),
+		ServerMode:  serverMode,
 	}
 }
