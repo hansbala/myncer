bf6e4e33e27558eab7664966eff61d28679b2970
Hans <hansbala@hansbala.com>
2025-05-31 18:11:30 -0700

    update not found page

--- myncer-web/src/pages/NotFound.tsx
+++ myncer-web/src/pages/NotFound.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom"
+
 export const NotFound = () => {
+  const navigate = useNavigate()
+
   return (
-    <div>Page not found</div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-4">
+      <h1 className="text-lg">Page not found</h1>
+      <Button onClick={() => navigate("/")}>Back to home</Button>
+    </div>
   )
 }
