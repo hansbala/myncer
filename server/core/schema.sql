1cdc7530aea53d7061b73661d3a73cefb5adf4e7
Hans <hansbala@hansbala.com>
2025-06-13 22:27:07 -0700

    add database schema for datasource tokens

--- server/core/schema.sql
+++ server/core/schema.sql
@@ -7,3 +7,14 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMPTZ DEFAULT now(),
     updated_at TIMESTAMPTZ DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS datasource_tokens (
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  datasource VARCHAR(64) NOT NULL,
+  -- Source of truth: Serialized OAuthToken proto.
+  data BYTEA NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT now(),
+  updated_at TIMESTAMPTZ DEFAULT now(),
+  UNIQUE (user_id, datasource)
+);
