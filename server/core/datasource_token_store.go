dd7762763baac53b89723b67c9f333ece7f7232d
Hans <hansbala@hansbala.com>
2025-06-14 19:31:21 -0700

    updates

--- server/core/datasource_token_store.go
+++ server/core/datasource_token_store.go
@@ -3,11 +3,17 @@ package core
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 
-	myncer_pb "github.com/hansbala/myncer/proto"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	myncer_pb "github.com/hansbala/myncer/proto"
+)
+
+var (
+	CErrTokenNotFound = NewError("oauth token not found")
 )
 
 type DatasourceTokenStore interface {
@@ -54,32 +60,31 @@ func (d *datasourceTokenStoreImpl) GetToken(
 	userId string,
 	datasource myncer_pb.Datasource,
 ) (*myncer_pb.OAuthToken, error) {
-	rows, err := d.db.QueryContext(
+	rows := d.db.QueryRowContext(
 		ctx,
-		`SELECT data, created_at, updated_at FROM datasource_tokens WHERE user_id = $1 AND datasource = $2 LIMIT 1`,
+		`SELECT data, created_at, updated_at
+		FROM datasource_tokens
+		WHERE user_id = $1 AND datasource = $2 LIMIT 1`,
 		userId,
 		datasource.String(),
 	)
-	if err != nil {
-		return nil, WrappedError(err, "failed to query datasource token")
-	}
-	defer rows.Close()
 
-	oAuthToken := &myncer_pb.OAuthToken{}
-	for rows.Next() {
-		var (
-			protoBytes []byte
-			createdAt time.Time
-			updatedAt time.Time
-		)
-		if err := rows.Scan(&protoBytes, &createdAt, &updatedAt); err != nil {
-			return nil, WrappedError(err, "failed to scan datasource token row")
+	var (
+		protoBytes []byte
+		createdAt  time.Time
+		updatedAt  time.Time
+		oAuthToken myncer_pb.OAuthToken
+	)
+	if err := rows.Scan(&protoBytes, &createdAt, &updatedAt); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, CErrTokenNotFound
 		}
-		if err := proto.Unmarshal(protoBytes, oAuthToken); err != nil {
-			return nil, WrappedError(err, "failed to unmarshal oauth token proto")
-		}
-		oAuthToken.CreatedAt = timestamppb.New(createdAt)
-		oAuthToken.UpdatedAt = timestamppb.New(updatedAt)
+		return nil, WrappedError(err, "failed to scan datasource token row")
 	}
-	return oAuthToken, nil
+	if err := proto.Unmarshal(protoBytes, &oAuthToken); err != nil {
+		return nil, WrappedError(err, "failed to unmarshal oauth token proto")
+	}
+	oAuthToken.CreatedAt = timestamppb.New(createdAt)
+	oAuthToken.UpdatedAt = timestamppb.New(updatedAt)
+	return &oAuthToken, nil
 }
