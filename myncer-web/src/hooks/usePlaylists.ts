ca2130870a803d9ecce97957ee038368014b559f
Hans <hansbala@hansbala.com>
2025-06-15 20:33:25 -0700

    usePlaylists hook

---
+++ myncer-web/src/hooks/usePlaylists.ts
@@ -0,0 +1,16 @@
+import type { Datasource, ListDatasourcePlaylistsResponse } from "@/generated_api/src";
+import { useApiClient } from "./useApiClient";
+import { useQuery } from "@tanstack/react-query";
+
+export const usePlaylists = ({ datasource }: { datasource: Datasource }) => {
+  const apiClient = useApiClient()
+  const { data: playlistsResponse, isLoading } = useQuery<ListDatasourcePlaylistsResponse>({
+    queryKey: ["playlists", "list", datasource],
+    queryFn: () => apiClient.listDatasourcePlaylists({ datasource: datasource }),
+    retry: false,
+  })
+  return {
+    playlists: playlistsResponse ? playlistsResponse.playlists || [] : [],
+    loading: isLoading,
+  }
+}
