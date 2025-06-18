29f2382cdd7387e649189d1ec477314a9a26194b
Hans <hansbala@hansbala.com>
2025-06-18 00:12:09 -0700

    add stubs

--- server/services/user_service.go
+++ server/services/user_service.go
@@ -5,7 +5,7 @@ import (
 
 	"connectrpc.com/connect"
 
-	// "github.com/hansbala/myncer/core"
+	"github.com/hansbala/myncer/core"
 	myncer_pb "github.com/hansbala/myncer/proto/myncer"
 	myncer_pb_connect "github.com/hansbala/myncer/proto/myncer/myncer_pbconnect"
 )
@@ -22,10 +22,34 @@ func (s *UserService) CreateUser(
 	ctx context.Context,
 	req *connect.Request[myncer_pb.CreateUserRequest], /*const*/
 ) (*connect.Response[myncer_pb.CreateUserResponse], error) {
-	// currUser := core.ToMyncerCtx(ctx).RequestUser
-	// core.DebugPrintJson(currUser)
-	res := connect.NewResponse(&myncer_pb.CreateUserResponse{
-		Greeting: "Hello from Hans!",
-	})
-	return res, nil
+	return nil, core.NewError("not implemented yet")
+}
+
+
+func (s *UserService) LoginUser(
+	context.Context, 
+	*connect.Request[myncer_pb.LoginUserRequest],
+) (*connect.Response[myncer_pb.LoginUserResponse], error) {
+	return nil, core.NewError("not implemented yet")
+}
+
+func (s *UserService) LogoutUser(
+	context.Context, 
+	*connect.Request[myncer_pb.LogoutUserRequest],
+) (*connect.Response[myncer_pb.LogoutUserResponse], error) {
+	return nil, core.NewError("not implemented yet")
+}
+
+func (s *UserService) EditUser(
+	context.Context,
+	*connect.Request[myncer_pb.EditUserRequest],
+) (*connect.Response[myncer_pb.EditUserResponse], error) {
+	return nil, core.NewError("not implemented yet")
+}
+
+func (s *UserService) GetCurrentUser(
+	context.Context, 
+	*connect.Request[myncer_pb.CurrentUserRequest],
+) (*connect.Response[myncer_pb.CurrentUserResponse], error) {
+	return nil, core.NewError("not implemented yet")
 }
