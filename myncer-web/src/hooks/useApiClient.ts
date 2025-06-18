dda086090cbd6886eafa8e88c3fea8eb1754c3de
Hans <hansbala@hansbala.com>
2025-06-18 00:06:27 -0700

    dep

--- myncer-web/src/hooks/useApiClient.ts
+++ myncer-web/src/hooks/useApiClient.ts
@@ -3,6 +3,9 @@ import { Configuration, DefaultApi } from "../generated_api/src"
 
 const BASE_PATH = import.meta.env.VITE_API_BASE_URL || 'https://myncer-api.hansbala.com/api/v1'
 
+/**
+ * @deprecated Use `useGrpcClient()` instead.
+ */
 export const useApiClient = () => {
   return useMemo(() => {
     const config = new Configuration({ basePath: BASE_PATH, credentials: 'include' })
