8ad644b3abaa5dfeb7045601ddbce26b5e9dc738
Hans <hansbala@hansbala.com>
2025-06-20 20:15:39 -0700

    move to constant file

---
+++ myncer-web/src/hooks/constants.ts
@@ -0,0 +1,1 @@
+export const GRPC_SERVER_URL = "http://localhost:6969"
--- myncer-web/src/hooks/useUserService.ts
+++ myncer-web/src/hooks/useUserService.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
 import { UserService } from "@/generated_grpc/myncer/user_pb";
-
-const serverUrl = "http://localhost:6969"
+import { GRPC_SERVER_URL } from "./constants";
 
 export const useUserService = () => {
   const transport = createConnectTransport({
-    baseUrl: serverUrl,
+    baseUrl: GRPC_SERVER_URL,
     fetch: (input: RequestInfo | URL, init?: RequestInit) => {
       return fetch(input, {
         ...init,
