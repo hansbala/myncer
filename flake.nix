3aac507ccfc3cb5015871da1ba3a2f8deaef65c3
Hans <hansbala@hansbala.com>
2025-06-22 16:29:27 -0700

    get rid of openjdk in the nix flake

--- flake.nix
+++ flake.nix
@@ -15,21 +15,11 @@
             pkgs.nodejs_20
             pkgs.pnpm
             pkgs.protobuf
-            pkgs.openjdk17
             pkgs.docker
           ];
 
           shellHook = ''
             export PATH=$PWD/node_modules/.bin:$PATH
-            export OPENAPI_GENERATOR_CLI_JAR="$PWD/thirdparty/openapi-generator-cli.jar"
-
-            if [ ! -f "$OPENAPI_GENERATOR_CLI_JAR" ]; then
-              echo "⬇️  Downloading OpenAPI Generator CLI JAR..."
-              mkdir -p thirdparty
-              curl -L -o "$OPENAPI_GENERATOR_CLI_JAR" https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.4.0/openapi-generator-cli-7.4.0.jar
-            fi
-
-            alias openapi-gen="java -jar $OPENAPI_GENERATOR_CLI_JAR"
             echo "🧪 Myncer flake shell ready"
           '';
         };
