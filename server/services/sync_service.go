950a2446fae8c2046158fa6262057427ecb66a5c
Hans <hansbala@hansbala.com>
2025-06-21 23:19:18 -0700

    hook up rest of handlers

--- server/services/sync_service.go
+++ server/services/sync_service.go
@@ -8,16 +8,23 @@ import (
 	myncer_pb "github.com/hansbala/myncer/proto/myncer"
 	myncer_pb_connect "github.com/hansbala/myncer/proto/myncer/myncer_pbconnect"
 	"github.com/hansbala/myncer/rpc_handlers"
+	"github.com/hansbala/myncer/sync_engine"
 )
 
 func NewSyncService() *SyncService {
 	return &SyncService{
 		createSyncHandler: rpc_handlers.NewCreateSyncHandler(),
+		deleteSyncHandler: rpc_handlers.NewDeleteSyncHandler(),
+		listSyncsHandler:  rpc_handlers.NewListSyncsHandler(),
+		runSyncHandler:    rpc_handlers.NewRunSyncHandler(sync_engine.NewSyncEngine()),
 	}
 }
 
-type SyncService struct{
-	createSyncHandler  core.GrpcHandler[*myncer_pb.CreateSyncRequest, *myncer_pb.CreateSyncResponse]
+type SyncService struct {
+	createSyncHandler core.GrpcHandler[*myncer_pb.CreateSyncRequest, *myncer_pb.CreateSyncResponse]
+	deleteSyncHandler core.GrpcHandler[*myncer_pb.DeleteSyncRequest, *myncer_pb.DeleteSyncResponse]
+	listSyncsHandler  core.GrpcHandler[*myncer_pb.ListSyncsRequest, *myncer_pb.ListSyncsResponse]
+	runSyncHandler    core.GrpcHandler[*myncer_pb.RunSyncRequest, *myncer_pb.RunSyncResponse]
 }
 
 var _ myncer_pb_connect.SyncServiceHandler = (*SyncService)(nil)
@@ -33,19 +40,19 @@ func (d *SyncService) DeleteSync(
 	ctx context.Context,
 	req *connect.Request[myncer_pb.DeleteSyncRequest], /*const*/
 ) (*connect.Response[myncer_pb.DeleteSyncResponse], error) {
-	return nil, core.NewError("not implemented")
+	return OrchestrateHandler(ctx, d.deleteSyncHandler, req.Msg)
 }
 
 func (d *SyncService) ListSyncs(
 	ctx context.Context,
 	req *connect.Request[myncer_pb.ListSyncsRequest], /*const*/
 ) (*connect.Response[myncer_pb.ListSyncsResponse], error) {
-	return nil, core.NewError("not implemented")
+	return OrchestrateHandler(ctx, d.listSyncsHandler, req.Msg)
 }
 
 func (d *SyncService) RunSync(
 	ctx context.Context,
 	req *connect.Request[myncer_pb.RunSyncRequest], /*const*/
 ) (*connect.Response[myncer_pb.RunSyncResponse], error) {
-	return nil, core.NewError("not implemented")
+	return OrchestrateHandler(ctx, d.runSyncHandler, req.Msg)
 }
