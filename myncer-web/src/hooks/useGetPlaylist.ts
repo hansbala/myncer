e103b0f263039fa8f6faab10b32e9a90855ec5e9
Hans <hansbala@hansbala.com>
2025-06-21 21:50:07 -0700

    use get playlist hook and cleanup

---
+++ myncer-web/src/hooks/useGetPlaylist.ts
@@ -0,0 +1,23 @@
+import { getPlaylistDetails } from "@/generated_grpc/myncer/datasource-DatasourceService_connectquery"
+import type { Datasource } from "@/generated_grpc/myncer/datasource_pb"
+import { useQuery } from "@connectrpc/connect-query"
+
+export const useGetPlaylist = ({ datasource, playlistId }: { datasource?: Datasource, playlistId?: string }) => {
+  const { data: response, isLoading, isError } = useQuery(
+    getPlaylistDetails,
+    {
+      datasource,
+      playlistId,
+    },
+    {
+      // Only fetch playlists if a datasource and playlist id is provided.
+      enabled: !!datasource && !!playlistId,
+      retry: false,
+    },
+  )
+
+  return {
+    playlist: isError ? undefined : response?.playlist,
+    loading: isLoading,
+  }
+}
