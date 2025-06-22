665a8184c86b67c3849ea56097ac9585642ba66e
Hans <hansbala@hansbala.com>
2025-06-21 21:43:40 -0700

    update ds selector

--- myncer-web/src/components/DatasourceSelector.tsx
+++ myncer-web/src/components/DatasourceSelector.tsx
@@ -5,8 +5,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Datasource } from "@/generated_grpc/myncer/datasource_pb"
 import { Controller, type UseControllerProps, type FieldValues } from "react-hook-form"
-import type { Datasource } from "@/generated_api/src"
 
 interface Props<T extends FieldValues> extends UseControllerProps<T> {
   datasources: Datasource[]
@@ -23,11 +23,24 @@ export function DatasourceSelector<T extends FieldValues>({ datasources, label, 
             <SelectValue placeholder={label} />
           </SelectTrigger>
           <SelectContent>
-            {datasources.map((ds) => (
-              <SelectItem key={ds} value={ds}>
-                {ds}
-              </SelectItem>
-            ))}
+            {datasources.map((ds) => {
+              let datasourceLabel: string
+              switch (ds) {
+                case Datasource.SPOTIFY:
+                  datasourceLabel = "Spotify"
+                  break
+                case Datasource.YOUTUBE:
+                  datasourceLabel = "YouTube"
+                  break
+                default:
+                  datasourceLabel = ds.toString()
+              }
+              return (
+                <SelectItem key={ds} value={datasourceLabel}>
+                  {datasourceLabel}
+                </SelectItem>
+              )
+            })}
           </SelectContent>
         </Select>
       )}
