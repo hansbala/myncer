1ca8f80c429876020a9925f124dfce6993f3a385
Hans <hansbala@hansbala.com>
2025-06-23 01:52:14 -0700

    some manual chunking for performance

--- myncer-web/vite.config.ts
+++ myncer-web/vite.config.ts
@@ -11,4 +11,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react")) return "react"
+            if (id.includes("@radix-ui")) return "radix"
+            if (id.includes("lucide-react")) return "icons"
+            if (id.includes("connect-query") || id.includes("protobuf")) return "connectrpc"
+            if (id.includes("react-query")) return "query"
+            if (id.includes("zod") || id.includes("react-hook-form")) return "forms"
+          }
+        },
+      },
+    },
+  }
 })
