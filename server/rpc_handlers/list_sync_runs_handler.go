4ea3d22a8d0710c6d17da61f05a61d4809cb49b8
Hans <hansbala@hansbala.com>
2025-06-23 01:46:14 -0700

    filter sync runs to current user

--- server/rpc_handlers/list_sync_runs_handler.go
+++ server/rpc_handlers/list_sync_runs_handler.go
@@ -32,12 +32,54 @@ func (l *listSyncRunsImpl) ProcessRequest(
 	userInfo *myncer_pb.User, /*const,@nullable*/
 	reqBody *myncer_pb.ListSyncRunsRequest, /*const*/
 ) *core.GrpcHandlerResponse[*myncer_pb.ListSyncRunsResponse] {
-	syncs, err := core.ToMyncerCtx(ctx).DB.SyncRunStore.GetSyncs(ctx, nil /*runIds*/, nil /*syncIds*/)
+	syncRuns, err := core.ToMyncerCtx(ctx).DB.SyncRunStore.GetSyncs(ctx, nil /*runIds*/, nil /*syncIds*/)
 	if err != nil {
 		return core.NewGrpcHandlerResponse_InternalServerError[*myncer_pb.ListSyncRunsResponse](
 			core.WrappedError(err, "failed to get sync runs from database"),
 		)
 	}
 
-	return core.NewGrpcHandlerResponse_OK(&myncer_pb.ListSyncRunsResponse{SyncRuns: syncs.ToArray()})
+	filteredSyncs, err := l.filterUserSyncs(ctx, syncRuns, userInfo)
+	if err != nil {
+		return core.NewGrpcHandlerResponse_InternalServerError[*myncer_pb.ListSyncRunsResponse](
+			core.WrappedError(err, "failed to filter sync runs for user"),
+		)
+	}
+
+	return core.NewGrpcHandlerResponse_OK(
+		&myncer_pb.ListSyncRunsResponse{SyncRuns: filteredSyncs.ToArray()},
+	)
+}
+
+func (l *listSyncRunsImpl) filterUserSyncs(
+	ctx context.Context,
+	syncRuns core.Set[*myncer_pb.SyncRun], /*const*/
+	userInfo *myncer_pb.User, /*const*/
+) (core.Set[*myncer_pb.SyncRun], error) {
+	userSyncIds, err := l.getUserSyncIds(ctx, userInfo)
+	if err != nil {
+		return nil, core.WrappedError(err, "failed to get user sync ids")
+	}
+	r := core.NewSet[*myncer_pb.SyncRun]()
+	for _, sync := range syncRuns.ToArray() {
+		if userSyncIds.Contains(sync.GetSyncId()) {
+			r.Add(sync)
+		}
+	}
+	return r, nil
+}
+
+func (l *listSyncRunsImpl) getUserSyncIds(
+	ctx context.Context,
+	userInfo *myncer_pb.User, /*const*/
+) (core.Set[string], error) {
+	userSyncs, err := core.ToMyncerCtx(ctx).DB.SyncStore.GetSyncs(ctx, userInfo)
+	if err != nil {
+		return nil, core.WrappedError(err, "failed to get user syncs")
+	}
+	r := core.NewSet[string]()
+	for _, sync := range userSyncs.ToArray() {
+		r.Add(sync.GetId())
+	}
+	return r, nil
 }
